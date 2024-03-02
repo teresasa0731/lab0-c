2db229dfd39aa85f943296f6835dff692d17e7cb
teresasa0731 <teresasa731@gmail.com>
2024-03-03 03:49:16 +0800

    implement q_free()

--- queue.c
+++ queue.c
@@ -22,7 +22,17 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head) {
+    if (!head)
+        return;
+
+    elememt_t *cur, *next;
+    list_for_each_entry_safe (cur, next, head, list)
+        q_release_element(cur);
+
+    free(head);
+}
+
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
